2ef2e208b8f6581511eb6efbf40c0a2954537fb1
leehlh@ha.org.hk <leehlh@ha.org.hk>
2024-11-22 11:29:34 +0800

    xxx

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,9 +1,22 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+// get inputs from action.yml
+  const bucket = core.getInput('bucket', {required: true})
+  const bucketRegion = core.getInput('bucket-region', {required: true})
+  const distFolder = core.getInput('dist-folder', {required: true})
+
+// upload files  
+// use AWS CLI to upload files to S3 bucket, CLI installed by default in Ubuntu runner
+  const s3Uri = `s3://${bucket}`
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
   core.notice('hi! my javascript github action')
+
+  // can get the action details by this syntax
+  // github.context.action
 }
 
 run();
